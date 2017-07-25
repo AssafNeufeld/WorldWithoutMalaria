9e4f4493a9522d7532f2b711042fde10055173fe
Barak Yaari <baya@microsoft.com>
2017-07-25 11:48:31 +0300

    Fixed merger stuff

--- nasa/data4/Merger.py
+++ nasa/data4/Merger.py
@@ -3,8 +3,8 @@ from os.path import isfile, join
 import sys
 import re
 
-path = sys.argv[1]
-
+# path = sys.argv[1]
+path = 'C:\\Barak\\WorldWithoutMalaria\\nasa\\example_data'
 
 m = re.search("MERRA2_300.(.*)_2d_slv_Nx.([0-9]{4})([0-9]{4}).txt", "MERRA2_300.statD_2d_slv_Nx.20041207.txt")
 type = m.group(1)
@@ -15,7 +15,7 @@ onlyfiles = [f for f in listdir(path) if isfile(join(path, f))]
 
 groups = {}
 for f in onlyfiles:
-    m = re.search("MERRA2_300.(.*)_2d_slv_Nx.([0-9]{4})([0-9]{4}).txt", f)
+    m = re.search("MERRA2_[0-9]00.(.*)_2d_slv_Nx.([0-9]{4})([0-9]{4}).txt", f)
     if m is None:
         continue
     type = m.group(1)
@@ -34,10 +34,11 @@ for year, dic in groups.items():
     with open(year + '.txt', 'a') as outFile:
         for day, data in dic.items():
             sdate = [year + "-" + day[:2] + "-" + day[2:]]
-            with open(path + join(path, data["tavg1"])) as tavg1:
-                with open(path + join(path, data["statD"])) as statD:
-                    sRow = statD.readline().split(',')
-                    tRow = tavg1.readline().split(',')
-                    out = sdate + [str(e) for e in sRow + tRow[2:]]
-                    s=','.join(out)+'\n'
-                    outFile.write(s)
+            with open(join(path, data["tavg1"]),'r') as tavg1:
+                with open(join(path, data["statD"]), 'r') as statD:
+                    for line in tavg1:
+                        sRow = statD.readline().split(',')
+                        tRow = line.split(',')
+                        out = sdate + [str(e) for e in sRow + tRow[2:]]
+                        s=','.join(out)
+                        outFile.write(s)
