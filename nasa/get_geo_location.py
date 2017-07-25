c48a67448f826e2eca9c53a81445b1b900f1899f
Barak Yaari <baya@microsoft.com>
2017-07-25 14:17:38 +0300

    Save

--- nasa/get_geo_location.py
+++ nasa/get_geo_location.py
@@ -2,21 +2,26 @@ import requests
 
 
 def get_location(lat, lon):
-    location_data = requests.get('http://maps.googleapis.com/maps/api/geocode/json?latlng={},{}&sensor=true'.format(lat, lon))
+    try:
+        location_data = requests.get('http://maps.googleapis.com/maps/api/geocode/json?latlng={},{}&sensor=true'.format(lat, lon))
 
-    json = location_data.json()['results'][0]['formatted_address']
-    print(json)
+        json = location_data.json()['results'][0]['formatted_address']
+        print(json)
+        return json
+    except:
+        return "None"
 
 get_location(14, 14.5)
 
 file = open('example_data.txt')
 lines = file.readlines()
 locations = []
-for line in lines:
+for line in lines[1:]:
     splitted = line.split(',')
     lat = splitted[0]
     lon = splitted[1]
     print(lat, lon)
     with open("locations" + '.txt', 'a') as outFile:
-        data = ','.join([lat, lon, get_location(lat, lon)])
-        print(data)
+        data = ';'.join([ lat, lon, get_location(lat, lon)])
+        outFile.write(data + '\n')
+
